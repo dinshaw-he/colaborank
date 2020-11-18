fec613407fdc81aaa591bb14fdf87e695665b22d
Dinshaw <dinshaw.gobhai@hotelengine.com>
2020-11-18 14:52:13 -0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,18 @@
-homer = User.create! email: 'homer@simpsons.com', password: 'password'
-marge = User.create! email: 'marge@simpsons.com', password: 'password'
-bart = User.create! email: 'bart@simpsons.com', password: 'password'
+homer = User.find_or_create_by(email: 'homer@simpsons.com') do |user|
+  user.password = 'password'
+end
+marge = User.find_or_create_by(email: 'marge@simpsons.com') do |user|
+  user.password = 'password'
+end
+bart = User.find_or_create_by(email: 'bart@simpsons.com') do |user|
+  user.password = 'password'
+end
 
-marge.pointed_events.create! value: 1000
-marge.pointed_events.create! value: 1000
-marge.pointed_events.create! value: 1000
+marge.pointed_events.create! value: 1000, type: 'PR_APPROVAL'
+marge.pointed_events.create! value: 1000, type: 'PR_APPROVAL'
+marge.pointed_events.create! value: 1000, type: 'PR_APPROVAL'
 
-homer.pointed_events.create! value: 1000
-homer.pointed_events.create! value: 1000
+homer.pointed_events.create! value: 1000, type: 'PR_APPROVAL'
+homer.pointed_events.create! value: 1000, type: 'PR_APPROVAL'
 
-bart.pointed_events.create! value: 1000
+bart.pointed_events.create! value: 1000, type: 'PR_APPROVAL'
