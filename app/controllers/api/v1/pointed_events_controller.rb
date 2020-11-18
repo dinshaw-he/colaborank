f0752c675e389095d9cb2d60105eac7d18ca29fa
Dinshaw <dinshaw.gobhai@hotelengine.com>
2020-11-18 15:10:14 -0700

    Fix app

--- app/controllers/api/v1/pointed_events_controller.rb
+++ app/controllers/api/v1/pointed_events_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class PointedEventsController < ApplicationController
       skip_before_action :authenticate_user!
-      skip_before_filter :verify_authenticity_token
+      skip_before_action :verify_authenticity_token
 
       def create
         user = User.find_by! email: email_params[:email]
