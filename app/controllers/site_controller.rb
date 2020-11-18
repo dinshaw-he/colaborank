d19bba282fd028289ec994b40b1964641b843e34
Dinshaw <dinshaw.gobhai@hotelengine.com>
2020-11-18 16:46:39 -0700

    Added github handle
    Store the User's github handle so we can identify them via github action.
    
    Co-authored-by: Dinshaw Gobhai <dinshaw.gobhai@hotelengine.com>
    Co-authored-by: Derek Ruggles <Derek@hotelengine.com>

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,11 +1,14 @@
 class SiteController < ApplicationController
-  Ranking = Struct.new :email, :total, keyword_init: true
+  Ranking = Struct.new :email, :github, :total, keyword_init: true
+
   def index
     point_events = PointedEvent.select('user_id, sum(value) as total').group('user_id').order('total desc')
 
     @rankings = point_events.map do |pointed_event|
+      user = User.find pointed_event.user_id
       Ranking.new(
-        email: User.find(pointed_event.user_id).email,
+        email: user.email,
+        github: user.github,
         total: pointed_event.total
       )
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 homer = User.find_or_create_by(email: 'homer@simpsons.com') do |user|
   user.password = 'password'
+  user.github = 'hsimpson'
 end
 marge = User.find_or_create_by(email: 'marge@simpsons.com') do |user|
   user.password = 'password'
+  user.github = 'msimpson'
 end
 bart = User.find_or_create_by(email: 'bart@simpsons.com') do |user|
   user.password = 'password'
+  user.github = 'bsimpson'
 end
 
 marge.pointed_events.create! value: 1000, type: 'PR_APPROVAL'
