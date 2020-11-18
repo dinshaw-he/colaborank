02ecae411915cccb4e9005c05aba9da32ec40056
notwillpiers <will.piers@hotelengine.com>
2020-11-18 16:53:02 -0700

    Add a chart

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "src/index"
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
