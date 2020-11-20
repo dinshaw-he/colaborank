f15589aaa5aaf3fbe0de3c992aabb31947587487
Dinshaw <dinshaw.gobhai@hotelengine.com>
2020-11-20 14:36:17 -0700

    Fix index total

--- app/controllers/pointed_events_controller.rb
+++ app/controllers/pointed_events_controller.rb
@@ -2,7 +2,9 @@ class PointedEventsController < ApplicationController
   def index
     @q = PointedEventsCollection.new(params: search_params).q
     @q.sorts = 'created_at desc' if @q.sorts.empty?
-    @pointed_events = @q.result(distinct: true).includes(:user).page params[:page]
+    all_pointed_events = @q.result(distinct: true)
+    @total_points = all_pointed_events.sum &:value
+    @pointed_events = all_pointed_events.includes(:user).page params[:page]
   end
 
   private
